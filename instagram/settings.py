cde06be02a129581afd2943a156bca969d563a6c
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-08 16:55:15 -0600

    updated settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "instagram/static",
+    BASE_DIR / 'instagram/static',
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
