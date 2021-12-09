0d29cbf24e7119de9feeacf9420dc940629f6494
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-08 18:07:43 -0600

    updated files

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth.apps.AuthConfig',
+    'authy.apps.AuthyConfig',
 ]
 
 MIDDLEWARE = [
