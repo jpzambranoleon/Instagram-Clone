916212b5cbfa153d3b51e55fd99b9eaa94dd7e7f
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-08 19:00:47 -0600

    updated urls.py

---
+++ authy/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as authViews 
+
+urlpatterns = [
+    path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+]
