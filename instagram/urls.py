55260b90c0337f567c637cede3271457efaad877
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-08 18:52:24 -0600

    updated urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import authy
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('authy.urls'))
 ]
