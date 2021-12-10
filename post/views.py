571e5cdca5fc96e333564657af119ee49f1905ed
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-09 18:21:06 -0600

    updated views.py

--- post/views.py
+++ post/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from post.models import Post, Stream
 
 # Create your views here.
+
+@login_required
+def index(request):
+    user = request.user
+    posts = Stream.objects.filter(user=user)
+
+    groups_ids = []
+
+    for post in posts:
+        groups_ids.append(post.post_id)
+
+    post_items = Post.objects.filter(id__in=groups_ids).all().order_by('-posted')
+
+    template = loader.get_template('index.html')
+
+    context = {
+        'post_items': post_items,
+    }
+
+    return HttpResponse(template.render(context, request))
